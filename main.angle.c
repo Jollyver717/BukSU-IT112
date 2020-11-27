8411c71a13cee23d645bc882cb1c3aea197e6d42
Jollyver717 <73811236+Jollyver717@users.noreply.github.com>
2020-11-27 15:30:01 +0800

    Add files via upload

---
+++ main.angle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int a,b,c,n;
+    printf("\n enter the three angles: ");
+    scanf("\ %d%d%d", &a, &b, &c);
+
+    if( n == 180)
+    {
+        printf("\n valid angles");
+    }
+     else
+    {
+        printf("n\ not valid angles");
+    }
+    return 0;
+}
