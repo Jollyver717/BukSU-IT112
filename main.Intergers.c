a3edcbbfe623cc311910e2f502b5bdaf487158e5
Jollyver717 <73811236+Jollyver717@users.noreply.github.com>
2020-12-05 11:26:59 +0800

    Add files via upload

---
+++ main.Intergers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    n = 1;
+    while(n<=10)
+    {
+        printf("%d %d\n , n,n*n");
+        n++;
+    }
+    getch();
+    return 0;
+}
